e70b57dfdf621f2c8c3ef1e5266504627957512d
Salman Alyahya <saalyahy@asu.edu>
2016-02-21 03:06:02 -0700

    The second version

--- CSE360Assignments/src/cse360assign3/Calculator.java
+++ CSE360Assignments/src/cse360assign3/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
 	 *  here the variable total get initializes to size 0
 	 *  */
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		total = 0;  
 	}
 	
 	/**This method return the value in the variable total. 
@@ -26,7 +26,7 @@ public class Calculator {
 	 * @return total 			the total of the calculation.
 	 * */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**This method add the given value to the private variable total. 
@@ -34,7 +34,7 @@ public class Calculator {
 	 * @param value 			the value to be added to the total.
 	 * */
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	/**This method subtract the given value from the private variable total. 
@@ -42,7 +42,7 @@ public class Calculator {
 	 * @param value 			the value to be subtract from the total.
 	 * */
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	/**This method multiply the given value to the private variable total. 
@@ -50,7 +50,7 @@ public class Calculator {
 	 * @param value 			the value to be multiplied to the total.
 	 * */
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	/**This method divide the given value by the private variable total. 
@@ -58,7 +58,10 @@ public class Calculator {
 	 * @param value 			the value to be divided by the total.
 	 * */
 	public void divide (int value) {
-		
+		if(value == 0)
+			total = 0;
+		else
+			total = total / value;
 	}
 	
 	/**This method return a history of all actions as a String. . 
--- CSE360Assignments/src/cse360assign3/CalculatorTest.java
+++ CSE360Assignments/src/cse360assign3/CalculatorTest.java
@@ -4,41 +4,81 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest 
+{
 
 	@Test
-	public void testCalculator() {
+	public void testCalculator() 
+	{
+		//checking if the constructor will be created.
 		Calculator cal = new Calculator();
 		assertNotNull(cal);
 	}
  
 	@Test
-	public void testGetTotal() {
-		fail("Not yet implemented");
+	public void testGetTotal() 
+	{
+		//checking if getTotal will return the correct total.
+		Calculator cal = new Calculator();
+		assertEquals(0, cal .getTotal());
+		cal.add(10);
+		assertEquals(10, cal .getTotal());
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAdd() 
+	{
+		//checking if add will add the correct value to the total.
+		Calculator cal = new Calculator();
+		cal.add(10);
+		assertEquals(10, cal .getTotal());
+		cal.add(8);
+		cal.add(1);
+		assertEquals(19, cal .getTotal());
 	}
 
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
+	public void testSubtract() 
+	{
+		//checking if Subtract will Subtract the correct value from the total.
+		Calculator cal = new Calculator();
+		cal.add(100);
+		assertEquals(100, cal .getTotal());
+		cal.subtract(19);
+		cal.subtract(22);
+		assertEquals(59, cal .getTotal());
 	}
 
 	@Test
-	public void testMultiply() {
-		fail("Not yet implemented");
+	public void testMultiply() 
+	{
+		//checking if Multiply will Multiply the total by the correct value.
+		Calculator cal = new Calculator();
+		cal.multiply(20);
+		assertEquals(0, cal .getTotal());
+		cal.add(2);
+		cal.multiply(3);
+		cal.multiply(5);
+		assertEquals(30, cal .getTotal());
 	}
 
 	@Test
-	public void testDivide() {
-		fail("Not yet implemented");
+	public void testDivide() 
+	{
+		//checking if Divide will Divide the total by the correct value, and that total will equal 0 if that value is 0.
+		Calculator cal = new Calculator();
+		cal.divide(5);
+		assertEquals(0, cal .getTotal());
+		cal.add(20);
+		cal.divide(5);
+		assertEquals(4, cal .getTotal());
+		cal.multiply(0);
+		assertEquals(0, cal .getTotal());
 	}
 
 	@Test
-	public void testGetHistory() {
+	public void testGetHistory() 
+	{
 		fail("Not yet implemented");
 	}
 
